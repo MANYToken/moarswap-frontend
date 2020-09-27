3557662fa62e19075c092a6d8adc6903b0be8b93
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-09-27 04:25:48 -0400

    force a build

--- www/src/views/Farm/components/Stake.tsx
+++ www/src/views/Farm/components/Stake.tsx
@@ -1,13 +1,13 @@
 import BigNumber from 'bignumber.js'
-import React, { useCallback, useState } from 'react'
+import React, {useCallback, useState} from 'react'
 import styled from 'styled-components'
-import { Contract } from 'web3-eth-contract'
+import {Contract} from 'web3-eth-contract'
 import Button from '../../../components/Button'
 import Card from '../../../components/Card'
 import CardContent from '../../../components/CardContent'
 import CardIcon from '../../../components/CardIcon'
 import IconButton from '../../../components/IconButton'
-import { AddIcon } from '../../../components/icons'
+import {AddIcon} from '../../../components/icons'
 import Label from '../../../components/Label'
 import Value from '../../../components/Value'
 import useAllowance from '../../../hooks/useAllowance'
@@ -17,7 +17,7 @@ import useStake from '../../../hooks/useStake'
 import useStakedBalance from '../../../hooks/useStakedBalance'
 import useTokenBalance from '../../../hooks/useTokenBalance'
 import useUnstake from '../../../hooks/useUnstake'
-import { getBalanceNumber } from '../../../utils/formatBalance'
+import {getBalanceNumber} from '../../../utils/formatBalance'
 import DepositModal from './DepositModal'
 import WithdrawModal from './WithdrawModal'
 
@@ -27,24 +27,24 @@ interface StakeProps {
   tokenName: string
 }
 
-const Stake: React.FC<StakeProps> = ({ lpContract, pid, tokenName }) => {
+const Stake: React.FC<StakeProps> = ({lpContract, pid, tokenName}) => {
   const [requestedApproval, setRequestedApproval] = useState(false)
 
   const allowance = useAllowance(lpContract)
-  const { onApprove } = useApprove(lpContract)
+  const {onApprove} = useApprove(lpContract)
 
   const tokenBalance = useTokenBalance(lpContract.options.address)
   const stakedBalance = useStakedBalance(pid)
 
-  const { onStake } = useStake(pid)
-  const { onUnstake } = useUnstake(pid)
+  const {onStake} = useStake(pid)
+  const {onUnstake} = useUnstake(pid)
 
   const [onPresentDeposit] = useModal(
     <DepositModal
       max={tokenBalance}
       onConfirm={onStake}
       tokenName={tokenName}
-    />,
+    />
   )
 
   const [onPresentWithdraw] = useModal(
@@ -52,7 +52,7 @@ const Stake: React.FC<StakeProps> = ({ lpContract, pid, tokenName }) => {
       max={stakedBalance}
       onConfirm={onUnstake}
       tokenName={tokenName}
-    />,
+    />
   )
 
   const handleApprove = useCallback(async () => {
@@ -73,7 +73,9 @@ const Stake: React.FC<StakeProps> = ({ lpContract, pid, tokenName }) => {
       <CardContent>
         <StyledCardContentInner>
           <StyledCardHeader>
-            <CardIcon>👨🏻‍🍳</CardIcon>
+            <CardIcon>
+              <span role={'img'}>👨🏻‍🍳</span>
+            </CardIcon>
             <Value value={getBalanceNumber(stakedBalance)} />
             <Label text={`${tokenName} Tokens Staked`} />
           </StyledCardHeader>
